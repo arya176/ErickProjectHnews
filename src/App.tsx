ab0532bb9d1f358cdbb1a2a97faedd6ebd1f107f
arya <aryaradfar@gmail.com>
2023-07-17 17:23:21 +1000

    add body component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import React from "react";
 // import "./App.css";
+import { Body } from "./components/body/Body";
 import { Header } from "./components/header/Header";
 
 function App() {
   return (
-    <div className="">
+    <div className="flex flex-col">
       <Header headline={"Top Stories From HackerNews By Arya"} />
+      <Body />
     </div>
   );
 }
---
+++ src/components/body/Body.tsx
@@ -0,0 +1,9 @@
+export const Body = () => {
+  return (
+    <div>
+      <div className="w-[800px] h-full">
+        <p>this is body</p>
+      </div>
+    </div>
+  );
+};
